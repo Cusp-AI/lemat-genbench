5410a8ffae529dfcf4ba9dfad419889b6912c621
JeanGabrielArgaud <jean-gabriel.argaud@entalpic.ai>
2025-11-25 11:45:42 +0100

    feat: unifying data_dir

--- src/lemat_genbench/utils/oxidation_state.py
+++ src/lemat_genbench/utils/oxidation_state.py
@@ -104,10 +104,11 @@ def compositional_oxi_state_guesses(
             )
 
     # Load prior probabilities of oxidation states, used to rank solutions
-    here = Path(__file__).resolve().parent
-    three_up = here.parents[2]
+    # Create data directory if it doesn't exist
+    data_dir = Path("data")
+    data_dir.mkdir(exist_ok=True)
 
-    with open(three_up / "data" / "lemat_icsd_oxi_dict_probs.json", "r") as f:
+    with open(data_dir /"lemat_icsd_oxi_dict_probs.json", "r") as f:
         loaded_dict = json.load(f)
     type(comp).oxi_prob = loaded_dict
     oxi_states_override = oxi_states_override or {}
